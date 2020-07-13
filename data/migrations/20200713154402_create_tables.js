ee1956ebffcd0b2772effb6161d35e054eb165d2
mlazzell17@gmail.com <MicheSi>
2020-07-13 15:59:44 -0700

    created tables

--- data/migrations/20200713154402_create_tables.js
+++ data/migrations/20200713154402_create_tables.js
@@ -1,8 +1,33 @@
 
 exports.up = function(knex) {
-  
+  return knex.schema
+    .createTable('users', tbl => {
+        tbl.increments();
+
+        tbl.string('username', 128)
+            .index()
+            .notNullable()
+            .unique();
+        tbl.string('password', 128).notNullable();
+    })
+
+    .createTable('dreams', tbl => {
+        tbl.increments();
+
+        tbl.date('date').notNullable();
+        tbl.text('description').notNullable();
+        tbl.integer('user_id')
+            .unsigned()
+            .notNullable()
+            .references('id')
+            .inTable('users')
+            .onUpdate('CASCADE')
+            .onDelete('CASCADE')
+    })
 };
 
 exports.down = function(knex) {
-  
+  return knex.schema
+    .dropTableIfExists('users')
+    .dropTableIfExists('dreams')
 };
