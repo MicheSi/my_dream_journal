003ccba33301943d9c0960f26779e9b2aaa37c02
mlazzell17@gmail.com <MicheSi>
2020-07-21 16:07:07 -0700

    added some styling to add dream form

--- client/src/components/dashboard.js
+++ client/src/components/dashboard.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import Nav from './nav';
+import NewDream from './newDream';
 
 const Dashboard = () => {
     return (
         <div className='dashboard'>
             <h1>This is your Dream Journal</h1>
-            <Nav />
+            <NewDream />
         </div>
     )
 }
