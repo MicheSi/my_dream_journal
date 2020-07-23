61b0dd31da7c9d25814bd3c5b87c8db2ac0f9480
mlazzell17@gmail.com <MicheSi>
2020-07-23 15:49:48 -0700

    added delete and update to users

--- users/users-router.js
+++ users/users-router.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 
 const Users = require('./users-model');
+const Dreams = require('../dreams/dreams-model');
 
 router.get('/', (req, res) => {
     Users.find()
@@ -26,10 +27,45 @@ router.get('/:id', (req, res) => {
         })
 })
 
-router.get('/:id/posts', (req, res) => {
+router.get('/:id/dreams', (req, res) => {
     Users.findUserDreams(req.params.id)
         .then(dreams => {
             res.status(200).json(dreams)
+        })
+        .catch(err => {
+            res.status(500).json({message: err})
+        })
+})
+
+router.post('/:id/dreams', (req, res) => {
+    const dreamInfo = {...req.body, user_id: req.params.id}
+
+    Dreams.add(dreamInfo)
+        .then(dream => {
+            res.status(201).json(dream)
+        })
+        .catch(err => {
+            res.status(500).json({message: err})
+        })
+})
+
+router.delete('/:id', (req, res) => {
+    Users.remove(req.params.id)
+        .then(user => {
+            res.status(200).json(user)
+        })
+        .catch(err => {
+            res.status(500).json({message: err})
+        })
+})
+
+router.put('/:id', (req, res) => {
+    Users.update(req.params.id, req.body)
+        .then(user => {
+            res.status(200).json(user)
+        })
+        .catch(err => {
+            res.status(500).json({message: err})
         })
 })
 
