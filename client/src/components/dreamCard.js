3cc83a8b59ca1bb863f0ef6015eb58ff6ea2a5d1
mlazzell17@gmail.com <MicheSi>
2020-07-28 13:07:50 -0700

    created functions to edit and delete dreams

--- client/src/components/dreamCard.js
+++ client/src/components/dreamCard.js
@@ -1,7 +1,56 @@
-import React from 'react';
-import { Button, Card } from 'semantic-ui-react'
+import React, { useState, useEffect } from 'react';
+import { Button, Card, Header, Icon, Modal } from 'semantic-ui-react';
+import AxiosWithAuth from '../utils/AxiosWithAuth';
 
 const DreamCard = props => {
+    const initialState = {
+        date: '',
+        description: ''
+    }
+
+    const [dream, setDream] = useState(initialState);
+    console.log(props)
+
+    // set props to selected dream
+    useEffect(() => {
+        const dreamToEdit = props
+
+        if (dreamToEdit) {
+            setDream(dreamToEdit)
+        }
+    }, [props])
+
+    const changeHandler = e => {
+        setDream({
+            ...dream,
+            [e.target.name]: e.target.value
+        })
+    }
+
+    // edit dream function
+    const editDream = e => {
+        e.preventDefault()
+        AxiosWithAuth()
+            .put()
+            .then(res => {
+                console.log('updated dream', res)
+                window.location.reload()
+            })
+            .catch(err => console.log('Cannot update dream', err))
+    }
+
+    // delete dream function
+    const deleteDream = e => {
+        e.preventDefault()
+        AxiosWithAuth()
+            .delete()
+            .then(res => {
+                console.log('deleted dream', res)
+                window.location.reload()
+            })
+            .catch(err => console.log('Unable to delete dream', err))
+
+    }
 
     return (
         <div className='dreamCard'>
@@ -14,9 +63,26 @@ const DreamCard = props => {
                 </Card.Content>
                 <Card.Content extra>
                     <div className='ui two buttons'>
-                        <Button basic color='violet'>
+                        {/* <Button basic color='violet'>
                             Edit
-                        </Button>
+                        </Button> */}
+                        <Modal trigger={<Button basic color='violet'>Edit</Button>} closeIcon>
+                            <Header icon='archive' content='Archive Old Messages' />
+                            <Modal.Content>
+                                <p>
+                                    Your inbox is getting full, would you like us to enable automatic
+                                    archiving of old messages?
+                                </p>
+                            </Modal.Content>
+                            <Modal.Actions>
+                                <Button color='red'>
+                                    <Icon name='remove' /> No
+                                </Button>
+                                <Button color='green'>
+                                    <Icon name='checkmark' /> Yes
+                                </Button>
+                            </Modal.Actions>
+                        </Modal>
                         <Button basic color='red'>
                             Delete
                         </Button>
