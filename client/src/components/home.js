d1ef49382054e060ae07eddfdcd06ef68a18020f
mlazzell17@gmail.com <MicheSi>
2020-08-07 14:01:15 -0700

    redesigned home page

--- client/src/components/home.js
+++ client/src/components/home.js
@@ -7,27 +7,28 @@ const Home = () => {
     return (
         <div className='homePage'>
             <h1>My Dream Journal</h1>
-            <p>Duis eu minim magna nulla sit commodo officia sit sunt id eiusmod. Cupidatat duis dolor id aliqua reprehenderit consectetur nulla. Cillum exercitation cillum nostrud esse laborum Lorem ipsum pariatur eiusmod cupidatat occaecat ut incididunt ullamco.
+            <div className='aboutDiv'>
+            <p className='about'>Duis eu minim magna nulla sit commodo officia sit sunt id eiusmod. Cupidatat duis dolor id aliqua reprehenderit consectetur nulla. Cillum exercitation cillum nostrud esse laborum Lorem ipsum pariatur eiusmod cupidatat occaecat ut incididunt ullamco.
                 on cupidatat occaecat et do deserunt adipisicing aliqua laboris nostrud exercitation. In cupidatat esse consequat labore ad amet in amet duis. Duis proident officia aliqua anim nisi dolore ad quis.
                 Eiusmod qui ipsum minim quis eiusmod deserunt sunt. Cillum et enim dolore consequat excepteur consequat ut. Ut quis tempor nulla nostrud et
             </p>
             <div className='buttonsDiv'>
-                <Button size ='big' animated>
+                <Button size='big' animated>
                     <Link to='/register'>
-                        <Button.Content visible>Register</Button.Content>
-                        <Button.Content hidden>
+                        <Button.Content className='btnText' visible>Register</Button.Content>
+                        <Button.Content className='btnText' hidden>
                             <Icon name='arrow right' />
                         </Button.Content>
                     </Link>
                 </Button>
                 <Button size ='big' animated='fade'>
                     <Link to='/signin'>
-                        <Button.Content visible>Already have an account?</Button.Content>
-                        <Button.Content hidden>Sign In</Button.Content>
+                        <Button.Content className='btnText' visible>Already have an account?</Button.Content>
+                        <Button.Content className='btnText' hidden>Sign In</Button.Content>
                     </Link>
                 </Button>
             </div>
-            
+            </div>
         </div>
     )
 }
