291ec3d43fcb0f16b99cc4fb29df98ee862cf1ce
mlazzell17@gmail.com <MicheSi>
2020-08-13 14:18:05 -0700

    changed text on home page

--- client/src/components/home.js
+++ client/src/components/home.js
@@ -9,18 +9,30 @@ const Home = () => {
             <h1>My Dream Journal</h1>
             <div className='aboutDiv'>
                 <p className='dream'>dream</p>
-                <p className='dreamDesc'>| drēm | noun</p>
+                <p className='dreamDesc'>/ drēm /</p>
+                <p className='noun'>noun</p>
                 <p className='about'>
                     a series of thoughts, images and sensations occurring in a person's mind during sleep
                 </p>
-                <div className='aboutApp'>
-                    <p className='dreamMeaning'>Do dreams have a deeper meaning?</p>
+                <div className='aboutDreams'>
+                    {/* <p className='dreamMeaning'>Do dreams have a deeper meaning?</p>
                     <p>
                         Some psychologists think dreams are just the result of random brain activity. 
                         Others think dreams do have a deeper meaning, agreeing with Sigmund Freud's theory of dream interpretation.
                         Freud believed that dream interpretation was the "royal road" to the unconscious. 
                         He thought all dreams are forms of wish fulfillment, revealing your deepest desires.
-                    </p>
+                    </p> */}
+                    <div className='aboutApp'>
+                        <p className='write'>Keeping a Dream Journal</p>
+                        <p>
+                            There are many benefits to keeping a dream journal.
+                            Recurring dreams or symbols can reveal dream patterns that may stem from your waking life. 
+                            Being aware of these patterns can help you gain a better understanding of yourself and your feelings. 
+                            It can even help you practice lucid dreaming because you are more aware of your dreams.
+                            Dreams can also fuel your creativity. Many famous writers, artists and inventors say that their ideas came from their dreams.
+                        </p>
+                        <p>Whatever your reason for wanting to keep a dream journal, </p>
+                    </div>
                 </div>
                 <div className='buttonsDiv'>
                     <Button size='big' animated>
