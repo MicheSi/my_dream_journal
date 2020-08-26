3ad435b7e4d78cda2fe55cb0be1a4ec39f7e884a
mlazzell17@gmail.com <MicheSi>
2020-08-26 14:18:22 -0700

    home page 600px media query

--- client/src/components/menu.js
+++ client/src/components/menu.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Dropdown, Menu } from 'semantic-ui-react';
+import { Dropdown } from 'semantic-ui-react';
 
 const MenuBar = () => (
   <div className='menuBar'>
@@ -8,10 +8,6 @@ const MenuBar = () => (
           <Dropdown.Item href='/'>Home</Dropdown.Item>
           <Dropdown.Item href='/dashboard'>My Dashboard</Dropdown.Item>
           <Dropdown.Item href='/resources'>Resources</Dropdown.Item>
-          {/* <Dropdown.Item>About</Dropdown.Item> */}
-          {/* <Dropdown.Divider />
-          <Dropdown.Header>Export</Dropdown.Header>
-          <Dropdown.Item>Share</Dropdown.Item> */}
         </Dropdown.Menu>
       </Dropdown>
   </div>
