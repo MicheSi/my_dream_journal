35856bca361aebac7c67261fbc6332c58e83ad13
mlazzell17@gmail.com <MicheSi>
2020-09-01 15:16:27 -0700

    next and prev working

--- client/src/components/dreamsList.js
+++ client/src/components/dreamsList.js
@@ -1,9 +1,8 @@
 import React, { useState, useEffect } from 'react';
-import { Pagination } from 'semantic-ui-react';
+import { Pagination, PaginationItem, PaginationLink } from 'reactstrap';
 import moment from 'moment';
 import AxiosWithAuth from '../utils/AxiosWithAuth';
 import DreamCard from './dreamCard';
-
 
 
 const DreamsList = props => {
@@ -22,10 +21,16 @@ const DreamsList = props => {
                 setDreams(res.data.dreamResults)
             })
             .catch(err => console.log('Cannot retrieve dreams', err))
-    }, [])
+    }, [page])
 
-    const onChange = (e, pageInfo) => {
-        setPage(pageInfo.page)
+    const prevPage = e => {
+        if (page > 0) {
+            setPage(page - 1)
+        }
+    }
+
+    const nextPage = e => {
+        setPage(page + 1)
     }
 
     return (
@@ -42,14 +47,41 @@ const DreamsList = props => {
                      description={dream.description}
                     />
                 )           
-})}
-            <Pagination
-             ellipsisItem={null}
-             activePage={page}
-             onChange={onChange}
-             totalPages={10}
-            />
+            })}
             </div>
+            <Pagination size='lg' aria-label="Page navigation example">
+                <PaginationItem>
+                    <PaginationLink onClick={prevPage} previous href="" />
+                </PaginationItem>
+                <PaginationItem>
+                    <PaginationLink href="#">
+                     1
+                    </PaginationLink>
+                </PaginationItem>
+                <PaginationItem>
+                    <PaginationLink href="#">
+                     2
+                    </PaginationLink>
+                </PaginationItem>
+                <PaginationItem>
+                    <PaginationLink href="#">
+                     3
+                    </PaginationLink>
+                </PaginationItem>
+                <PaginationItem>
+                    <PaginationLink href="#">
+                     4
+                    </PaginationLink>
+                </PaginationItem>
+                <PaginationItem>
+                    <PaginationLink href="#">
+                     5
+                    </PaginationLink>
+                </PaginationItem>
+                <PaginationItem>
+                    <PaginationLink onClick={nextPage} next href="" />
+                </PaginationItem>
+            </Pagination>
         </div>
     )
 }
