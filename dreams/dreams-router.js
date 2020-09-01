0a693760f82a9582c1a08885175e26c8b7891dad
mlazzell17@gmail.com <MicheSi>
2020-09-01 11:42:36 -0700

    tried middleware but commented code out

--- dreams/dreams-router.js
+++ dreams/dreams-router.js
@@ -28,7 +28,7 @@ router.get('/:id', (req, res) => {
         })
 })
 
-// router.get('/users/:id', (req, res) => {
+// router.get('/users/:id', paginatedResults(Dreams), (req, res) => {
 //     const id = req.params.id;
 
 //     Dreams.findByUser(id)
@@ -129,5 +129,37 @@ router.delete('/:id', (req, res) => {
             res.status(500).json({message: err})
         })
 })
+
+
+// function paginatedResults(model) {
+//     return (req, res, next) => {
+//         const page = parseInt(req.query.page)
+//         const limit = parseInt(req.query.limit)
+
+//         const startIndex = (page - 1) * limit
+//         const endIndex = page * limit
+
+//         const results = {}
+
+//         if (endIndex < model.length) {
+//             results.next = {
+//                 page: page + 1,
+//                 limit: limit
+//             }
+//         }
+    
+//         if (startIndex > 0) {
+//             results.prev = {
+//                 page: page - 1,
+//                 limit: limit
+//             }
+//         }
+
+//         results.results = model.slice(startIndex, endIndex)
+
+//         res.paginatedResults = results
+//         next
+//     }
+// }
 
 module.exports = router;
