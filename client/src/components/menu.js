84604b49f336b1806c901289b558fc0e0117ccae
mlazzell17@gmail.com <MicheSi>
2020-09-03 13:34:31 -0700

    made function to reroute based on if user is logged in

--- client/src/components/menu.js
+++ client/src/components/menu.js
@@ -2,6 +2,17 @@ import React from 'react';
 import { Dropdown } from 'semantic-ui-react';
 
 const MenuBar = () => {
+  const loggedIn = localStorage.getItem('token')
+
+  const dashboard = () => {
+    if (loggedIn) {
+      window.location.href='/dashboard'
+    }
+    else {
+      window.location.href='signin'
+    }
+  }
+
   const signOut = () => {
     localStorage.clear();
     window.location.href='/'
@@ -14,7 +25,7 @@ const MenuBar = () => {
           <Dropdown.Item href='/'>Home</Dropdown.Item>
           <Dropdown.Item href='/register'>Register</Dropdown.Item>
           <Dropdown.Item href='/signin'>Sign In</Dropdown.Item>
-          <Dropdown.Item href='/dashboard'>My Dashboard</Dropdown.Item>
+          <Dropdown.Item onClick={dashboard}>My Dashboard</Dropdown.Item>
           <Dropdown.Item href='/resources'>Resources</Dropdown.Item>
           <Dropdown.Item onClick={signOut}>Sign Out</Dropdown.Item>
         </Dropdown.Menu>
