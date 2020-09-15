5668fad2976718d59d8aa089e813a7f88796cbe2
mlazzell17@gmail.com <MicheSi>
2020-09-15 15:03:23 -0700

    add dream actions

--- client/src/actions/dreamActions.js
+++ client/src/actions/dreamActions.js
@@ -11,3 +11,39 @@ export const EDIT_DREAMS_FAILED = 'EDIT_DREAM_FAILED'
 export const DELETE_DREAMS_LOADING = 'DELETE_DREAM_LOADING'
 export const DELETE_DREAMS_SUCCESS = 'DELETE_DREAM_SUCCESS'
 export const DELETE_DREAMS_FAILED = 'DELETE_DREAM_FAILED'
+
+export const fetchDreams = () => dispatch => {
+    dispatch({type: FETCH_DREAMS_LOADING})
+    AxiosWithAuth()
+    .post('')
+    .then(res => {
+        dispatch({type: FETCH_DREAMS_SUCCESS, payload: res.data})
+    })
+    .catch(err => {
+        dispatch({type: FETCH_DREAMS_FAILED, payload: err.response})
+    })
+}
+
+export const editDreams = () => dispatch => {
+    dispatch({type: EDIT_DREAMS_LOADING})
+    AxiosWithAuth()
+    .post('')
+    .then(res => {
+        dispatch({type: EDIT_DREAMS_SUCCESS, payload: res.data})
+    })
+    .catch(err => {
+        dispatch({type: EDIT_DREAMS_FAILED, payload: err.response})
+    })
+}
+
+export const deleteDreams = () => dispatch => {
+    dispatch({type: DELETE_DREAMS_LOADING})
+    AxiosWithAuth()
+    .post('')
+    .then(res => {
+        dispatch({type: DELETE_DREAMS_SUCCESS, payload: res.data})
+    })
+    .catch(err => {
+        dispatch({type: DELETE_DREAMS_FAILED, payload: err.response})
+    })
+}
