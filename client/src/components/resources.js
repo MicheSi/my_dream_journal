e0518de3857ffa8df26c24c9aaaf6cee147957cb
mlazzell17@gmail.com <MicheSi>
2020-09-23 12:32:26 -0700

    made menu static

--- client/src/components/resources.js
+++ client/src/components/resources.js
@@ -1,11 +1,10 @@
 import React from 'react';
-import MenuBar from './menu';
 
 const Resources = () => {
     return (
         <div className='resourcesDiv'>
             <header className='resourceHeader'>
-                <MenuBar />
+                
             </header> 
             <h1>Coming soon!</h1>
             {/* <p className='dreamMeaning'>Do dreams have a deeper meaning?</p>
