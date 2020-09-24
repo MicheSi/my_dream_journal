a2b88295b3c2ba5b9e55fe061a602109e5325160
mlazzell17@gmail.com <MicheSi>
2020-09-24 13:04:59 -0700

    working on resources page

--- client/src/components/resources.js
+++ client/src/components/resources.js
@@ -4,16 +4,18 @@ const Resources = () => {
     return (
         <div className='resourcesDiv'>
             <header className='resourceHeader'>
-                
+                <h1 className='dreamMeaning'>Do dreams have a deeper meaning?</h1>
+                <p>
+                    Some psychologists think dreams are just the result of random brain activity. 
+                    Others think dreams do have a deeper meaning, agreeing with Sigmund Freud's theory of dream interpretation.
+                    Freud believed that dream interpretation was the "royal road" to the unconscious. 
+                    He thought all dreams are forms of wish fulfillment, revealing your deepest desires.
+                </p>
             </header> 
-            <h1>Coming soon!</h1>
-            {/* <p className='dreamMeaning'>Do dreams have a deeper meaning?</p>
-                    <p>
-                        Some psychologists think dreams are just the result of random brain activity. 
-                        Others think dreams do have a deeper meaning, agreeing with Sigmund Freud's theory of dream interpretation.
-                        Freud believed that dream interpretation was the "royal road" to the unconscious. 
-                        He thought all dreams are forms of wish fulfillment, revealing your deepest desires.
-                    </p> */}
+            <div>
+
+            </div>
+                    
         </div>
     )
 }
