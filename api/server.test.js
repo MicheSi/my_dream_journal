d29d1c666f0f1b911b73e82c1847f706675cacc4
mlazzell17@gmail.com <MicheSi>
2020-10-20 14:09:56 -0700

    working on auth-router tests

--- api/server.test.js
+++ api/server.test.js
@@ -9,18 +9,18 @@ describe('server', function () {
     describe('GET /', function () {
         it('response 200 on success', function () {
             return request(server)
-            .get('/')
-            .then(res => {
-                expect(res.status).toBe(200)
-            })
+                .get('/')
+                .then(res => {
+                    expect(res.status).toBe(200)
+                })
         })
 
         it('should return Server is running!', function () {
             return request(server)
-            .get('/')
-            .then(res => {
-                expect(res.text).toEqual('Server is running!')
-            })
+                .get('/')
+                .then(res => {
+                    expect(res.text).toEqual('Server is running!')
+                })
         })
     })
 })
--- auth/auth-router.test.js
+++ auth/auth-router.test.js
@@ -1,3 +1,48 @@
 const request = require('supertest');
 const server = require('../api/server');
 
+describe('auth router', function () {
+    it('tests are running', function () {
+        expect(true).toBe(true)
+    })
+
+    describe('POST /api/auth/register', function () {
+        it('status 201 on successful register', function () {
+            return request(server)
+                .post('/api/auth/register')
+                .send({
+                    username: 'TestUsername',
+                    password: 'testPassword'
+                })
+                .then( res => {
+                    expect(res.status).toBe(201)
+                })
+        })
+    })
+
+    describe('POST /api/auth/login', function () {
+        it('returns JSON on successful login', function () {
+            return request(server)
+                .post('/api/auth/login')
+                .send({
+                    username: 'TestUsername',
+                    password: 'testPassword'
+                })
+                .then(res => {
+                    expect(res.type).toBe('text/html')
+                })
+        })
+
+        it('returns a welcome message upon successful login', function () {
+            return request(server)
+                .post('/api/auth/login')
+                .send({
+                    username: 'TestUsername',
+                    password: 'testPassword'
+                })
+                .then(res => {
+                    expect(res.body.message).toBe('Welcome TestUsername')
+                })
+        })
+    })
+})
